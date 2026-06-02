d642642402269a6239c7fe213ab641e4681bda12
Umair Khurshid <umairkhurshidshaheen@gmail.com>
2026-06-02 06:32:55 +0500

    fix: remove jekyll-last-modified-at and harden custom lastmod hook
    - Removes jekyll-last-modified-at from Gemfile and plugins list
      (it crashes on theme gem static files that lack git history)
    - Updates custom posts-lastmod-hook.rb to check File.exist? and
      redirect git stderr before running git commands, so it gracefully
      skips files not tracked in git

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -3,22 +3,24 @@
 # Check for changed posts and pages
 
 Jekyll::Hooks.register :posts, :post_init do |post|
+  next unless File.exist?(post.path)
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  commit_num = `git rev-list --count HEAD "#{ post.path }" 2>/dev/null`
 
   if commit_num.to_i > 0
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }" 2>/dev/null`
     post.data['last_modified_at'] = lastmod_date
   end
 
 end
 
 Jekyll::Hooks.register :pages, :post_init do |page|
+  next unless File.exist?(page.path)
 
-  commit_num = `git rev-list --count HEAD "#{ page.path }"`
+  commit_num = `git rev-list --count HEAD "#{ page.path }" 2>/dev/null`
 
   if commit_num.to_i > 0
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ page.path }"`
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ page.path }" 2>/dev/null`
     page.data['last_modified_at'] = lastmod_date
   end
 
