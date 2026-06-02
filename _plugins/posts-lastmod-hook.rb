7e19ebcfb08b07c5ecdd55da865e80ea06bf56c7
Umair Khurshid <umairkhurshidshaheen@gmail.com>
2026-06-02 06:28:29 +0500

    feat: extend last-modified hook to cover static pages
    The custom Ruby hook previously only ran on :posts. Now it also
    registers for :pages, so the sitemap can include accurate <lastmod>
    for static pages like about.md and index.html.

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -1,14 +1,25 @@
 #!/usr/bin/env ruby
 #
-# Check for changed posts
+# Check for changed posts and pages
 
 Jekyll::Hooks.register :posts, :post_init do |post|
 
   commit_num = `git rev-list --count HEAD "#{ post.path }"`
 
-  if commit_num.to_i > 1
+  if commit_num.to_i > 0
     lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
     post.data['last_modified_at'] = lastmod_date
+  end
+
+end
+
+Jekyll::Hooks.register :pages, :post_init do |page|
+
+  commit_num = `git rev-list --count HEAD "#{ page.path }"`
+
+  if commit_num.to_i > 0
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ page.path }"`
+    page.data['last_modified_at'] = lastmod_date
   end
 
 end
